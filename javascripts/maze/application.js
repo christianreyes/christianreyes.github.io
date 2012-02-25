d18dc61d03d284e57548635d6513ad129036e865
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-02-25 13:18:06 -0500

    added maze.html points to vps for back-end

---
+++ javascripts/maze/application.js
@@ -0,0 +1,353 @@
+/* Assassin's Maze
+ *
+ * Christian Reyes
+ * Carnegie Mellon University
+ * 05433D Software Structures for User Interfaces - Web Lab
+ *
+ * Final Project
+ *
+ * Uses node.js and socket.io with raphaeljs for cross-browser SVG/VML graphics
+ *
+ */
+
+// global variable for disabling keyboard when animating.
+var _not_animating = true;
+
+$(function(){
+  
+  /* 
+  ============================
+        INITIALIZATION
+  ============================
+  */
+  
+  var my_client_id;
+  
+  // needed for resizing the paper
+  // all drawing operations happen in the raphael canvas, similar to HTML5 canvas
+  var paper = new ScaleRaphael("canvas_container", 780, 660);
+  
+  // function to resize the paper 
+  function resizePaper(){
+        var win = $(this);
+        paper.changeSize(win.width(), win.height(), true, false);
+  }    
+  
+  // resize the paper before drawing
+  $(window).resize(resizePaper);
+
+  // initialize the maze
+  var MAZE = new Maze(paper, 33, 39, "full", 20);
+  
+  // some variables used to manage the game
+  var blood;
+  var my_circle;
+  var mask;
+  var assassin_id;
+  var others = {};
+  
+  // translates key input to x and y differences
+  var key_to_xy = { 
+                    /* up   */  38: { x:  0, y: -1 }, 
+                    /* down */  40: { x:  0, y:  1 }, 
+                    /* left */  37: { x: -1, y:  0 }, 
+                    /* right */ 39: { x:  1, y:  0 } 
+                    };
+  
+  // translates key input to r and c differences
+  // r = row c = column
+  var key_to_rc = {
+                  /* up   */  38: { r: -1, c:  0 }, 
+                  /* down */  40: { r:  1, c:  0 }, 
+                  /* left */  37: { r:  0, c: -1 }, 
+                  /* right */ 39: { r:  0, c:  1 }
+                  };
+  
+  /* 
+  ============================
+         SOCKET LOGIC
+  ============================
+  */
+  
+  // connect to server. use auto-discovery for port
+  var socket = io.connect("http://datareyes.com:3000");
+  
+  // store the socket_id in my_client_id
+  socket.on("connect", function(){
+    log("CONNECTED!");
+    
+    my_client_id = socket.socket.sessionid;
+  });
+  
+  // server tells client if they are assassin or not
+  // when client is told, client makes self
+  socket.on("you are", function(data){
+    var rc = MAZE.randomCellRC();
+
+    // create the circle on the screen based on whether or not client is assassin
+    my_circle = new Circle( my_client_id , MAZE,  rc.r, rc.c, randomColor(), data.assassin, true );
+    
+    if(data.assassin){
+      assassin_id = my_client_id;
+    }
+    
+    // prepare data to be sent to the server after creation
+    var data = { 
+                 client_id: my_client_id,
+                 color:     my_circle.color,
+                 position:  {r: my_circle.row, c: my_circle.col},
+                 assassin: my_circle.assassin
+                };
+    
+    resizePaper();
+    
+    // show the instructions after all this has happened. slideup when the x is clicked to close
+    $('.instructions').slideDown(1000, "swing", function(){
+      $('.instructions .close').fadeIn(function(){
+        $(this).click(function(){
+          $('.instructions').slideUp();
+        });
+      });
+    });
+    
+    // process the keydown. move circle and then determine if a kill was made or not
+    $(window).bind("keydown", function(e){
+      // only continue if not currently animating
+      if(_not_animating){
+        // cross-browser support for keycode
+        var key = e.keyCode ? e.keyCode : e.which ;
+
+        //if the key is a valid key for moving: up down left right
+        if( key in key_to_xy) {
+          var rc_diffs =  key_to_rc[ key ]; // get the rc diffs
+
+          if( my_circle.canMove(rc_diffs) ) { // move circle if can move
+            var xy_diffs = key_to_xy[ key ];
+
+            // move the circle to the new absolute position
+            my_circle.moveTo({ r: my_circle.row + rc_diffs.r,
+                               c: my_circle.col + rc_diffs.c });
+
+            // prepare move data to send to server to have the other 
+            // clients update this client's position
+            var move_data = { 
+                          client_id: my_client_id,
+                          key: key,
+                          position: {r: my_circle.row, c: my_circle.col}
+                        };
+
+            log("i moved");
+            log(move_data);
+
+            // send the move information to the server. will broadcast to everyone else.
+            socket.emit('i moved', move_data);
+
+            // if circle is assassin, check if any other circles are in that location. if so,
+            // kill them and tell everyone that they were killed
+            if(my_circle.assassin) { 
+              for(client_id in others){ // check if any of the other circles are in the same position as client
+                var other = others[client_id];
+                if(my_circle.row == other.row && my_circle.col == other.col){
+                  log("killed: " + client_id);
+                  
+                  // change self from looking like assassin to looking like a client
+                  my_circle.changeType(false);
+
+                  // generate a random, valid path location to send the other circle to
+                  var new_rc = MAZE.randomCellRC();
+                  other.killed(false, new_rc); // kill the circle and send them to the location
+                  
+                  assassin_id = client_id; // the other circle is now the assassin
+
+                  // prepare the kill data to send to server
+                  var kill_data = {
+                                    assassin_id: my_client_id,
+                                    target_id: client_id,
+                                    new_rc: new_rc
+                                   }; 
+                                   
+                  bloodSplat(paper); // display blood on screen
+
+                  socket.emit("killed", kill_data); // send kill data to server to relay to all clients
+                }
+              }
+            } else {
+              // if client is not assassin, check to see if they stepped where an assassin is.
+              // if so, kill this client and make it into an assassin.
+              
+              var assassin = others[assassin_id];
+              
+              if(my_circle.row == assassin.row && my_circle.col == assassin.col){
+                log("killed: " + my_client_id);
+
+                assassin.changeType(false);
+
+                var new_rc = MAZE.randomCellRC();
+                my_circle.killed(true, new_rc);
+                
+                assassin_id = my_client_id;
+
+                var kill_data = {
+                                  assassin_id: assassin_id,
+                                  target_id: my_client_id,
+                                  new_rc: new_rc
+                                 }; 
+                                 
+                bloodSplat(paper);
+
+                socket.emit("killed", kill_data);
+              }
+            }
+          }
+        }
+      }
+    });
+                          
+    log("my client id " + my_client_id)
+    socket.emit("add me to users", data);
+    
+    // fade in the maze after everything is ready
+    $('#canvas_container').fadeIn(3000);
+  });
+  
+  // if a circle is killed, check to see if it is this client
+  // if it is, update accordingly, if it is not, check to see if it's any client we know
+  // update accordingly
+  socket.on("killed", function (data){
+    if(data.target_id == my_client_id){
+      others[data.assassin_id].changeType(false);
+      my_circle.killed(true, data.new_rc); 
+      assassin_id = my_client_id;
+      bloodSplat(paper);
+    }
+    if( typeof(others[data.target_id]) != "undefined" && typeof(others[data.assassin_id]) != "undefined" ){
+      my_circle.changeType(false);
+      others[data.assassin_id].changeType(false);
+      others[data.target_id].killed(false, data.new_rc);
+      bloodSplat(paper);
+      assassin_id = data.target_id;
+    }
+  });
+  
+  // create all the circles for the current users in the game
+  socket.on("current users", function (data){ 
+    log("current_users");
+    log(data);
+    
+    for(client_id in data){
+      if(typeof(others[client_id]) == "undefined"){
+        var user_data = data[client_id];
+      
+        add_other_user(user_data);
+      }
+    }
+
+  });
+
+  // add circle to maze when a new user connects
+  socket.on("new user connected", function (data){
+    log("new user connected");
+    log(data);
+    
+    $("#new_user").text("user connected: " + data.client_id);
+    
+    add_other_user(data);
+  });
+  
+  // update the circle's position on the maze
+  socket.on('user moved', function (data) {
+    log("user moved");
+    log(data);
+    
+    var circle = others[data.client_id];
+    
+    circle.moveTo(data.position);
+  });
+  
+  // remove the circle from the maze and tracking
+  socket.on("user disconnected", function (data){
+    log("user disconnected");
+    log(data);
+    
+    removeOther(data);
+  });
+  
+  // update the assassin to the new one.
+  socket.on("new assassin", function(data){
+    assassin_id = data.assassin_id;
+    
+    if( data.assassin_id == my_client_id ){
+      my_circle.changeType(true);
+    } else {
+      if(typeof(others[data.assassin_id]) != "undefined"){
+        others[data.assassin_id].changeType(true);
+      } 
+    }
+  })
+   
+  // remove everyone from maze on disconnect
+  socket.on("disconnect", function (){
+    log("server disconnected");
+    
+    for(client_id in others){
+      removeOther({client_id: client_id});
+    }
+     
+    my_circle.element.animate({opacity: 0}, 1000, function(){
+      my_circle.element.remove();
+    });
+  });
+  
+  /* 
+  ============================
+           HELPERS
+  ============================
+  */
+  
+  // called when adding other circles
+  function add_other_user(data){
+    if( data.client_id != my_client_id && typeof(others[data.client_id]) == "undefined" ){
+      var other_circle = new Circle( data.client_id , 
+                                     MAZE,  
+                                     data.position.r, 
+                                     data.position.c, 
+                                     data.color, 
+                                     data.assassin, 
+                                     false );
+      
+      others[data.client_id] = other_circle;
+      
+      if(data.assassin){
+        assassin_id = data.client_id;
+      }
+    }
+  }
+  
+  // remove circle from maze, stop tracking that circle
+  function removeOther(data){
+    others[data.client_id].element.animate({opacity: 0}, 1000, function(){
+      this.remove();
+      delete others[data];
+    });
+  }
+  
+});
+
+// used to control console output in one place. set LOG = true to turn on console output again
+function log(data){
+  var LOG = false;
+
+  if(LOG){
+    console.log(data);
+  }
+}
+
+// generate a random valid color
+function randomColor(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+// helper to return the xy for a rc for a given maze
+function rc_to_xy(maze, row, col){
+  return { x: col * maze.cell_width, y: row * maze.cell_width };
+}
---
+++ javascripts/maze/circle.js
@@ -0,0 +1,145 @@
+// code for a circle object
+
+function Circle(id, maze, row, col, color, assassin, mask){
+  this.id = id;
+  this.color = color;
+  this.row = row;
+  this.col = col;
+  this.get_x = function(){
+    return this.col * maze.cell_width;
+  }
+  this.get_y = function(){
+    return this.row * maze.cell_width;
+  }
+  
+  this.assassin = assassin;
+  var fill_color = assassin ? "#333" : "#fff" ; //#fff if good #333 if bad
+  
+  /*
+   * Create the circle on the screen
+   */ 
+                              
+  var good = maze.paper.set(); 
+  var top = maze.paper.path("M16.779,5.693 c-1.298-2.981-4.265-5.068-7.723-5.068c-3.72,0-6.841,2.424-7.961,5.768C4.552,8.592,13.32,8.181,16.779,5.693z");
+      top.attr({fill: fill_color});
+  var bottom = maze.paper.path("M0.937,11.21 c0.958,3.606,4.211,6.277,8.12,6.277c4.02,0,7.368-2.816,8.214-6.582C13.569,13.185,4.858,13.693,0.937,11.21z");
+      bottom.attr({fill: fill_color});
+  var middle = maze.paper.path("M17.271,10.905 c0.135-0.598,0.217-1.211,0.217-1.849c0-1.198-0.257-2.333-0.708-3.363C13.32,8.181,4.552,8.592,1.096,6.393 C0.814,7.234,0.625,8.118,0.625,9.057c0,0.75,0.13,1.464,0.312,2.153C4.858,13.693,13.569,13.185,17.271,10.905z"); 
+      middle.attr({fill: color, stroke: "#444", "stroke-width": .5 });
+  good.push(top);
+  good.push(bottom);
+  good.push(middle);
+  
+  var location = rc_to_xy(maze, row, col);
+  
+  // need to transform the set because we cannot manipulate an x and y. sets do not have an x and y
+  
+  good.attr({transform: "t " + location.x + "," + location.y});
+  
+  // create the mask if the circle is for the user on this client
+  if(mask){
+    this.mask = maze.paper.image( assassin ? "images/maze/mask-bad.png" : "images/maze/mask-good.png" ,
+                                 location.x + maze.cell_width / 2 - 775, 
+                                 location.y + maze.cell_width / 2 - 775, 
+                                 1550, 1550);
+  }
+   
+  // used to change the type of circle from assassin to target or target to assassin
+  this.changeType = function(is_assassin){
+    this.assassin = is_assassin;
+    
+    // change the mask to a red one for assassin, clear one for target
+    if( typeof(this.mask) != "undefined"){
+      this.mask.attr({src: is_assassin ? "images/maze/mask-bad.png" : "images/maze/mask-good.png"});
+    }
+    
+    // animate changing the fill color of the circle
+    var fill_color = is_assassin ? "#333" : "#fff" ; //#fff if good #333 if bad
+    top.animate({ fill: fill_color }, 300);
+    bottom.animate({ fill: fill_color }, 300);
+  }
+  
+  // store the raphael object so that we can use it and manipulate it later
+  this.element = good;
+  
+  // determines if the circle can move based on a given maze
+  this.canMove = function(rc_diffs){
+    var newR = this.row + rc_diffs.r;
+    var newC = this.col + rc_diffs.c;
+
+    return ! maze.isWall(newR, newC);
+  };
+  
+  // move the circle and mask if applicable by transforming them
+  this.move = function(xy_diffs, rc_diffs){
+    this.row += rc_diffs.r;
+    this.col += rc_diffs.c;
+
+    this.element.attr({ transform: "...t " +
+                                          maze.cell_width * xy_diffs.x
+                                          + ", " + 
+                                          maze.cell_width * xy_diffs.y
+                        });
+                
+    if( typeof(this.mask) != "undefined" ){
+      this.mask.attr({ 
+                    x: this.mask.attrs.x + maze.cell_width * xy_diffs.x, 
+                    y: this.mask.attrs.y + maze.cell_width * xy_diffs.y
+                  });
+    }
+  };
+  
+  // take in an absolute position and transform the circle and mask to there
+  this.moveTo = function(rc_position){
+    this.row = rc_position.r;
+    this.col = rc_position.c;
+    
+    var xy = rc_to_xy(maze, rc_position.r, rc_position.c);
+
+    this.element.attr({ transform: "t " +
+                                          xy.x
+                                          + ", " + 
+                                          xy.y
+                        });
+                
+    if( typeof(this.mask) != "undefined" ){
+      this.mask.attr({ 
+                    x: xy.x + maze.cell_width / 2 - 775,
+                    y: xy.y + maze.cell_width / 2 - 775
+                  });
+    }
+  };
+  
+  // send circle to new position and change its type
+  // do not allow the keyboard to be used during the move if the client is killed
+  this.killed = function(me, new_rc){
+    var new_xy = rc_to_xy(maze, new_rc.r, new_rc.c);
+    
+    this.row = new_rc.r;
+    this.col = new_rc.c;
+    
+    var this_circle = this;
+    
+    if( me ){
+      _not_animating = false;
+    }
+    
+    this.element.animate({ transform: "t " +
+                                          new_xy.x
+                                          + ", " + 
+                                          new_xy.y
+                        }, 500, "bounce", function(){  
+                                              this_circle.changeType(true);
+                                              if( me ){
+                                                _not_animating = true;
+                                              }
+                                          });
+                
+    if( typeof(this.mask) != "undefined" ){
+      this.mask.animate({ 
+                    x: new_xy.x + maze.cell_width / 2 - 775,
+                    y: new_xy.y + maze.cell_width / 2 - 775,
+                   }, 100, "bounce");
+    }
+  }
+}
