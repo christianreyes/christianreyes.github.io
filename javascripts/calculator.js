7cd6846d463749feb00445aeab81fbdbc314af67
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-02-29 14:47:13 -0500

    fixed calc buttons

--- javascripts/calculator.js
+++ javascripts/calculator.js
@@ -5,19 +5,42 @@ $(function(){
     if(key >= 48 && key <= 57){
       display(display() + String(key - 48));
     } else if(key == 8){
-      display(display().substring(0,display().length-1))
+      back();
     }
   });
+  
   
   $('button.num').click(function(){
-    $('p').text($('p').text() + $(this).text());
+    num_pressed($(this).text());
   });
   
-  function display(val){
-    if(val){
-      $('p').text(val);
+  $('#larrow').click(function(){
+    back();
+  });
+});
+
+function display(val){
+  if(val){
+    $('p').text(val);
+  } else {
+    return $('p').text();
+  }
+}
+
+function num_pressed(num){
+  if(display() == "0"){
+    display(num);
+  } else {
+    display( display() + String(num) );
+  }
+}
+
+function back(){
+  if(display != "0"){
+    if( display().length == 1){
+      display("0");
     } else {
-      return $('p').text();
+      display( display().substring(0, display().length - 1 ));
     }
   }
-});
+}
