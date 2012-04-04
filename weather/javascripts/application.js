b914fd3fad36529b9ee02cc75f4941a2c2b271f1
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-04-03 22:22:31 -0400

    weather test

---
+++ weather/javascripts/application.js
@@ -0,0 +1,12 @@
+$(function(){
+  // ready function
+  navigator.geolocation.getCurrentPosition(function(position){
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    
+    $('#latitude').text(latitude);
+    $('#longitude').text(longitude);
+    
+    $('#weather').load("http://forecast.weather.gov/MapClick.php?lat=" + latitude + "&lon=" + longitude + "&FcstType=dwml")
+  });
+});
