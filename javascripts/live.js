7cf7c10bbde4e738e61a7bfadb2a30da813c7adb
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-05-26 17:12:56 -0500

    delayed following

--- javascripts/live.js
+++ javascripts/live.js
@@ -1,10 +1,29 @@
 $(function(){
   var circle = $('#circle');
+  var x,y;
   
   $(window).mousemove(function(e){
-    var x = e.clientX;
-    var y = e.clientY;
+    x = e.clientX;
+    y = e.clientY;
     
-    circle.css("top", y + "px").css("left", x + "px");
+    //circle.css("top", y + "px").css("left", x + "px");
   });
+  
+  setInterval(function(){
+    var bezier_params = {
+        start: { 
+          x: parseInt(circle.css("left")), 
+          y: parseInt(circle.css("top"))//, 
+          //angle: 10
+        },  
+        end: { 
+          x:x,
+          y:y//, 
+          //angle: -10, 
+          //length: 0.25
+        }
+      }
+
+    circle.animate({path : new $.path.bezier(bezier_params)}, 900);
+  }, 1000);
 });
