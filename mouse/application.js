c6c3758bbab9245b64b327133625d8caf930e45c
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-07-21 17:41:39 -0700

    mouse

--- mouse/application.js
+++ mouse/application.js
@@ -1,4 +1,6 @@
 $(function(){
+  $(body).append($('<div>').text("Page loaded"));
+  
   window.ondevicemotion = function(event) {
     $(body).append($('<div>').text(event.acceleration.x + '\t' + event.acceleration.y));
   }  
