89fb52f8ee0c2918fe05014d07c52f4468291909
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-09-25 22:36:46 -0400

    fixed paren

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,7 +2,7 @@ $(function(){
   var myRequest = new panoramio.PhotoRequest({
     'tag': 'sunset',
     'rect': {'sw': {'lat': -30, 'lng': 10.5}, 'ne': {'lat': 50.5, 'lng': 30}}
-  };
+  });
   
   console.log(myRequest);
 })
