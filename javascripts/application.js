99c49572fb20837a2ca669483d9addd1a02bae45
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-09-25 22:39:45 -0400

    sunset

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,6 @@
 $(function(){
   var myRequest = new panoramio.PhotoRequest({
-    'tag': 'sunset',
-    'rect': {'sw': {'lat': -30, 'lng': 10.5}, 'ne': {'lat': 50.5, 'lng': 30}}
+    'tag': 'sunset'
   });
   
   console.log(myRequest);
