ab9cd229f2da70783aa5d075b371272d08b69ec2
Christian Reyes <cjreyes@andrew.cmu.edu>
2013-04-26 01:29:00 -0400

    shop

--- hci/js/main.js
+++ hci/js/main.js
@@ -4,7 +4,14 @@ $(function(){
     
     var loadPages = function(){
         var tasks = [];
-        var pages = ["phone", "tutorial1", "tutorial2", "tutorial3", "shop"];
+        var pages = [
+                        "phone", 
+                        "tutorial1",
+                        "tutorial2", 
+                        "tutorial3", 
+                        "shop",
+                        "offer1"
+                    ];
         
         $(pages).each(function(ind, obj){
             var task = function(callback){
@@ -41,8 +48,6 @@ $(function(){
             var dest = $(this).attr("href");
             $(this).closest('div[data-role="page"]').hide();
             $(dest).show();
-            //$('div[data-role="page"]').html(tempData[dest]);
-            //$('div[data-role="page"]').attr('id', dest);
         });      
         
         $(".tutorial1").swipe( {
@@ -99,6 +104,13 @@ $(function(){
             //Default is 75px, set to 0 for demo so any distance triggers swipe
             threshold:20
         });
+        
+        $('ul.rewards li').click(function(e){
+            e.preventDefault();
+            var dest = $(this).attr("data-target");
+            $(this).closest('div[data-role="page"]').hide();
+            $(dest).show();
+        })
     };
     
     loadPages();
@@ -113,9 +125,7 @@ $(function(){
     
     var points = parseInt($('#points h2').text());
     
-    $('ul.rewards li').click(function(){
-        location.href = "offer1.html"
-    })
+    
     
     $('.offerdetail').swipe( {
             //Generic swipe handler for all directions
@@ -142,65 +152,6 @@ $(function(){
         window.location = "shop.html"
     });
     
-    /*
-    $('#splash form').submit(function(e){
-        e.preventDefault();
-        if($('#txtandrewid').val() == 'test') { 
-           //$.mobile.changePage('index.html#rewards');
-           window.location = "rewards.html"
-        } else {
-           //$.mobile.changePage('index.html#phone');
-           window.location = "phone.html"
-        }
-        return false;
-    });
-    */
-    
-    /*
-    $('#phone form').submit(function(e){
-        e.preventDefault();
-        window.location = "tutorial1.html"
-        return false;
-    });
-    */
-    
-    $(".tutorial1").swipe( {
-            //Generic swipe handler for all directions
-            swipe:function(event, direction, distance, duration, fingerCount) {
-              if(direction == "left")
-                window.location = "tutorial2.html"
-              if(direction == "right")
-                window.location = "index.html"
-            },
-            //Default is 75px, set to 0 for demo so any distance triggers swipe
-             threshold:20
-          });
-
-    $(".tutorial2").swipe( {
-      //Generic swipe handler for all directions
-      swipe:function(event, direction, distance, duration, fingerCount) {
-        if(direction == "left")
-          window.location = "tutorial3.html"
-        if(direction == "right")
-          window.location = "tutorial1.html"
-      },
-      //Default is 75px, set to 0 for demo so any distance triggers swipe
-       threshold:20
-    });
-
-    $(".tutorial3").swipe( {
-      //Generic swipe handler for all directions
-      swipe:function(event, direction, distance, duration, fingerCount) {
-        if(direction == "left"){
-            alert("Congrats! You have completed enrollment!")
-            window.location = "shop.html"   
-        }
-        if(direction == "right")
-          window.location = "tutorial2.html"
-      },
-      //Default is 75px, set to 0 for demo so any distance triggers swipe
-       threshold:20
-    });
     
     $('.offerdetail button').click(function(){
         /*
@@ -218,35 +169,4 @@ $(function(){
     setTimeout(function(){
                 $('#first').fadeIn();
     }, 1000)
-    
-    /*
-    $.get('login.html', function(data){
-        alert(data)
-    })
-    */
-    
-    /*
-    setTimeout(function(){
-        var fetch = $('<div></div>').load('phone.html')
-        
-        fetch.css({
-          width: "100%",
-          position: 'absolute',
-          top: 0,
-          left: (window.innerWidth)+'px'
-        });
-        
-        $('#wrapper').append(fetch);
-        
-        $('div[data-role="page"]:first').animate({
-            left: -(window.innerWidth)+'px'
-        }, 300)
-        
-        $(fetch).animate({
-            left: 0
-        }, 300)
-        
-        
-    }, 2000);
-    */
 });
